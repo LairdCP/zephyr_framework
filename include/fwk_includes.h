87a7de0ce2958ab127630aa8c74e033aea5252c5
Andrew Hedin <andrew.hedin@lairdconnect.com>
2022-08-09 16:07:49 -0500

    Add framework include header file
    Bug 22428
    This file doesn't need to be in every framework user folder.
    
    Signed-off-by: Andrew Hedin <andrew.hedin@lairdconnect.com>

---
+++ include/fwk_includes.h
@@ -0,0 +1,33 @@
+/**
+ * @file fwk_includes.h
+ * @brief Headers used with Message Framework when
+ * CONFIG_FWK_AUTO_GENERATE_FILES is true
+ *
+ * Copyright (c) 2022 Laird Connectivity
+ *
+ * SPDX-License-Identifier: LicenseRef-LairdConnectivity-Clause
+ */
+#ifndef __FWK_INCLUDES_H__
+#define __FWK_INCLUDES_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************/
+/* Includes                                                                   */
+/******************************************************************************/
+#include "Framework.h"
+#include "BufferPool.h"
+#include "FrameworkMsg.h"
+#include "FrameworkMacros.h"
+
+#include "framework_ids.h"
+#include "framework_msgcodes.h"
+#include "framework_types.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __FWK_INCLUDES_H__ */
