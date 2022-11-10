17a6683fd5d13fd7f89b0ec5f529da9dba95de39
Andrew Hedin <andrew.hedin@lairdconnect.com>
2022-11-10 08:17:31 -0600

    Add reset to default assertion handler
    Bug 23085
    If assertions are enabled, the default should be a reset.
    
    Signed-off-by: Andrew Hedin <andrew.hedin@lairdconnect.com>

--- source/FrameworkStubs.c
+++ source/FrameworkStubs.c
@@ -12,7 +12,13 @@ LOG_MODULE_REGISTER(framework_stubs, CONFIG_FRAMEWORK_LOG_LEVEL);
 /******************************************************************************/
 /* Includes                                                                   */
 /******************************************************************************/
+#include <sys/reboot.h>
+
 #include "Framework.h"
+
+#if defined(CONFIG_LCZ_SOFTWARE_RESET)
+#include <lcz_software_reset.h>
+#endif
 
 /******************************************************************************/
 /* Global Function Definitions                                                */
@@ -23,6 +29,13 @@ __weak void Framework_AssertionHandler(char *file, int line)
 	UNUSED_PARAMETER(line);
 
 	LOG_ERR("assertion: line: %d %s", line, file);
+
+#if defined(CONFIG_LCZ_SOFTWARE_RESET)
+	lcz_software_reset_after_assert(0);
+#else
+	sys_reboot(SYS_REBOOT_WARM);
+#endif
+
 }
 
 __weak bool Framework_InterruptContext(void)
